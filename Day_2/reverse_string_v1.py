d2c092d0c06022f39753e915acd12d26d5ddb2d1
Kiran Raj R <kiranrajvjd@gmail.com>
2025-12-23 23:36:30 +0530

    Day2: Reverse a string
    Two-pointer approach to reversing a string.

---
+++ Day_2/reverse_string_v1.py
@@ -0,0 +1,22 @@
+# Problem: 3
+# Problem: Reverse a string
+# Author: Kiranraj R.
+# Data: 23/12/2025
+# --------------------------------------------
+# Time complexity: O(n)
+# Space complexity: O(n)
+
+
+def reverseString(string):
+    arrStr = list(string)
+    i, j = 0, len(arrStr) - 1
+
+    while i < j:
+        arrStr[i], arrStr[j] = arrStr[j], arrStr[i]
+        i += 1
+        j -= 1
+
+    return "".join(arrStr)
+
+
+print(reverseString("kiran"))
---
+++ Day_2/reverse_string_v2.py
@@ -0,0 +1,15 @@
+# Problem: 4
+# Problem: Reverse a string
+# Author: Kiranraj R.
+# Data: 23/12/2025
+# --------------------------------------------
+# Time complexity: O(n)
+# Space complexity: O(n)
+
+
+def reverseString_v2(string):
+    return string[::-1]
+
+
+def reverseString_v3(string):
+    return "".join(reversed(string))
