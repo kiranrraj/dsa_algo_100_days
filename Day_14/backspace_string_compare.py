fec2e6baea7b0849b4e9b1c729c70ffb7bb34c37
Kiran Raj R <kiranrajvjd@gmail.com>
2026-01-07 14:16:13 +0530

    Day:14 Stack Problem
    Backspace String Compare

---
+++ Day_14/backspace_string_compare.py
@@ -0,0 +1,77 @@
+# Problem: 46
+# Problem: Backspace String Compare
+# Author: Kiranraj R.
+# Data: 04/01/2026
+# --------------------------------------------
+# Given two strings s and t, where # means “backspace” (delete the previous character if any),
+# return true if they become equal after processing.
+# --------------------------------------------
+# Time Complexity: O(n + m)
+# Space Complexity: O(1)  # NOTE: optimized approach uses only pointers + counters (no extra stack)
+
+
+def backspaceCompare_v2(s: str, t: str) -> bool:
+    # Start from the end
+    i, j = len(s) - 1, len(t) - 1
+    # To track skip count
+    skip_count_s, skip_count_t = 0, 0
+
+    #
+    # NOTE: This outer loop continues until BOTH strings are fully processed.
+    # We use OR because one string may still have pending characters/backspaces even if the other ended.
+    while i >= 0 or j >= 0:
+        while i >= 0:
+
+            # Case: 1
+            # If char is # then increment skip count, reduce i
+            if s[i] == "#":
+                skip_count_s += 1
+                i -= 1
+
+            # Case: 2
+            # if skip count greater than 0, means the char is not #
+            # and we have pending to delete, reduce skip count and reduce the
+            # value of i so the character is skipper for comparison.
+            elif skip_count_s > 0:
+                skip_count_s -= 1
+                i -= 1
+            # Case: 3
+            # Char is not # or nothing pending to delete, process the character as
+            # it is
+            else:
+                break
+        # Same as above
+        while j >= 0:
+            if t[j] == "#":
+                skip_count_t += 1
+                j -= 1
+            elif skip_count_t > 0:
+                skip_count_t -= 1
+                j -= 1
+            else:
+                break
+
+        # if we have pending char to be processed and
+        # current comparison fails, we know string is different.
+
+        if i >= 0 and j >= 0:
+            # Case A:
+            # Both pointers are pointing to valid characters
+            if s[i] != t[j]:
+                return False
+        elif i >= 0 or j >= 0:
+            # Case B:
+            # Exactly one pointer is valid -> mismatch in remaining valid characters
+            return False
+
+        # move both pointers left to continue searching for the next valid characters.
+        i -= 1
+        j -= 1
+
+    return True
+
+
+print(backspaceCompare_v2("ab#c", "ad#c"))
+print(backspaceCompare_v2("ab##", "c#d#"))
+print(backspaceCompare_v2("a#c", "b"))
+print(backspaceCompare_v2("a##c", "#a#c"))
---
+++ Day_14/backspace_string_compare_v2.py
@@ -0,0 +1,37 @@
+# Problem: 46
+# Problem: Backspace String Compare
+# Author: Kiranraj R.
+# Data: 04/01/2026
+# --------------------------------------------
+# Given two strings s and t, where # means “backspace” (delete the previous character if any),
+# return true if they become equal after processing.
+# --------------------------------------------
+# Time Complexity: O(n + m)
+# Space Complexity: O(n + m)
+
+
+def backspaceCompare_v1(s: str, t: str) -> bool:
+    string_1_stack = []
+    string_2_stack = []
+
+    for ch in s:
+        if ch == "#":
+            if string_1_stack:
+                string_1_stack.pop()
+        else:
+            string_1_stack.append(ch)
+
+    for ch in t:
+        if ch == "#":
+            if string_2_stack:
+                string_2_stack.pop()
+        else:
+            string_2_stack.append(ch)
+
+    return string_1_stack == string_2_stack
+
+
+print(backspaceCompare_v1("ab#c", "ad#c"))
+print(backspaceCompare_v1("ab##", "c#d#"))
+print(backspaceCompare_v1("a#c", "b"))
+print(backspaceCompare_v1("a##c", "#a#c"))
